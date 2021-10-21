09a41396b7d2fb36a727e71ee070d26eba2a326f
Gizem Tekiner <gizem.tekiner@bilgesgt.com>
2021-10-21 14:25:42 +0300

    merhaba3

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('LALALAf')
+    print_hi('LALALAfhhhfghfhfhfhf')
+    print('naber')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
